b39ce9fa2ddd52c532f60d9308d0acd62d06343b
dee-makes-sketches <dsadhikarivips@gmail.com>
2025-09-01 21:30:06 +0530

    Create two-sum-II.py

---
+++ two-pointer/two-sum-II.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        i,j=0,len(numbers)-1
+        while i<j:
+           current_sum=numbers[i]+numbers[j]
+           if current_sum==target:
+               return [i+1,j+1]
+           elif current_sum<target:
+                i+=1
+           else:
+                j-=1
